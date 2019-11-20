e8cc9c579c984977cec5b0bf70cfd1376ceacf28
Platiplus <quazarweb@gmail.com>
2019-11-19 22:38:36 -0300

    Logging and better security implemented

--- auth/app.js
+++ auth/app.js
@@ -1,14 +1,22 @@
 // DEPENDENCIES
 const bodyParser = require('body-parser')
 const express = require('express')
+const helmet = require('helmet')
+const fileSystem = require('fs')
+const logger = require('morgan')
+const path = require('path')
 const cors = require('cors')
 const app = express()
 const Database = require('./utils/Database')
+
+const accessLogStream = fileSystem.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' })
 
 // CONTROLLERS
 const authRoutes = require('./api/routes/auth-routes')
 
 // MIDDLEWARES
+app.use(helmet())
+app.use(logger('combined', { stream: accessLogStream }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
