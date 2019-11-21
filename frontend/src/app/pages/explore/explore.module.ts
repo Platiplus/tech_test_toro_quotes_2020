f937c5a33e33d1fae0020631fd154c3cebd40edd
Platiplus <quazarweb@gmail.com>
2019-11-21 15:13:20 -0300

    Profile and Stock explore screens implemented

--- frontend/src/app/pages/explore/explore.module.ts
+++ frontend/src/app/pages/explore/explore.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { ExploreComponent } from './explore.component';
+import { RouterModule } from '@angular/router';
+import { NavbarModule } from 'src/app/components/navbar/navbar.module';
 
 @NgModule({
-  imports: [ ],
+  imports: [
+    RouterModule,
+    NavbarModule,
+  ],
   declarations: [
     ExploreComponent,
   ],
--- frontend/src/app/pages/profile/profile.module.ts
+++ frontend/src/app/pages/profile/profile.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
-
 import { ProfileComponent } from './profile.component';
+import { RouterModule } from '@angular/router';
+import { NavbarModule } from 'src/app/components/navbar/navbar.module';
 
 @NgModule({
-  imports: [ ],
+  imports: [
+    RouterModule,
+    NavbarModule,
+  ],
   declarations: [
     ProfileComponent,
   ],
