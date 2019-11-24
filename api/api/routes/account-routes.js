6bb1782b425187ce7281e113321b91fa35e06d4a
Platiplus <quazarweb@gmail.com>
2019-11-24 17:06:04 -0300

    Docker compose changes

--- api/api/routes/account-routes.js
+++ api/api/routes/account-routes.js
@@ -3,10 +3,9 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/account-controller')
 const model = require('../models/validation-model')
-const auth = require('../middlewares/authentication-middleware')
 
 router.post('/:id', celebrate(model.accountCreateModel), controller.create)
-router.get('/:id', auth, celebrate(model.accountReadModel), controller.read)
-router.patch('/:id', auth, celebrate(model.accountUpdateModel), controller.update)
+router.get('/:id', celebrate(model.accountReadModel), controller.read)
+router.patch('/:id', celebrate(model.accountUpdateModel), controller.update)
 
 module.exports = router
--- api/api/routes/user-routes.js
+++ api/api/routes/user-routes.js
@@ -3,10 +3,9 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/user-controller')
 const model = require('../models/validation-model')
-const auth = require('../middlewares/authentication-middleware')
 
 router.post('/', celebrate(model.userCreateModel), controller.create)
-router.get('/:id', auth, celebrate(model.userReadModel), controller.read)
-router.patch('/:id', auth, celebrate(model.userUpdateModel), controller.update)
+router.get('/:id', celebrate(model.userReadModel), controller.read)
+router.patch('/:id', celebrate(model.userUpdateModel), controller.update)
 
 module.exports = router
